020fac3488a78f67ff9c42377274457e9fb233f4
Cristian Sanchez <crisanz89@gmail.com>
2024-05-15 20:04:59 -0300

    Eliminando archivo obsoleto

--- js/probabilidad.js
+++
@@ -1,6 +0,0 @@
-function probabilidadChi(probabilidad) {
-    if (!(isNaN(probabilidad))) {
-        let valorProbabilidad = [0.001, 0.0025, 0.0005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5];
-        return valorProbabilidad[probabilidad];
-    }
-}
